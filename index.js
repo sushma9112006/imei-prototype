a9f6d0b73ebe61a35932e443bbbd8319b8868af5
sushma9112006 <sushma_csj@cs.iitr.ac.in>
2026-04-07 10:44:57 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import routes from "./routes.js";
+
+const app = express();
+app.use(express.json());
+
+app.use("/api", routes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
